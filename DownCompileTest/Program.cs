e2d0029ebe2ed253777325437e71ce3b97e076dc
Caleb Serafin <calebserafin@outlook.com>
2023-05-10 20:04:59 +0200

    initial commit

---
+++ DownCompileTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DownCompileTest; 
+internal class Program {
+    static void Main(string[] args) {
+        ExammpleClass bob = new("Hello World", 5);
+        (string printableText, int aNumber) = bob;
+        Console.WriteLine(
+            $$"""
+            {
+                "name": {{bob}}
+            }
+            """);
+        // Pause before exit
+        Console.ReadKey();
+    }
+    record class ExammpleClass(string Text, int Number);
+}
